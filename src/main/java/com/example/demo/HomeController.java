ea0b918e8ce1b9344973e454819acfac94e45cd1
raphzam <raphaeljasonzamora@gmail.com>
2020-08-12 17:01:01 -0400

    update pizza

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -45,40 +45,64 @@ public class HomeController {
         return "orderForm";
     }
 
+    @RequestMapping("/addToOrder/{id}")
+    public String addToOrder(@PathVariable("id") long orderID, Model model) {
+        model.addAttribute("currentOrder", orderRepository.findById(orderID).get());
+        return "orderForm";
+    }
+
+    @RequestMapping("/updatepizza/{id}")
+    public String updatePizza(@PathVariable("id") long id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).get());
+
+        model.addAttribute("doughs", ingredientRepository.findAllByType(typeRepository.findByName("dough")));
+        model.addAttribute("proteins", ingredientRepository.findAllByType(typeRepository.findByName("protein")));
+        model.addAttribute("veggies", ingredientRepository.findAllByType(typeRepository.findByName("veggie")));
+        model.addAttribute("sauces", ingredientRepository.findAllByType(typeRepository.findByName("sauce")));
+        model.addAttribute("toppings", ingredientRepository.findAllByType(typeRepository.findByName("topping")));
+
+        return "orderForm";
+    }
+
     @PostMapping("/processOrder")
-    public String processPizzaOrder(@ModelAttribute("pizza") Pizza pizza, Model model, @RequestParam("dough") long id, Principal principal) {
-        Ingredient ing = ingredientRepository.findById(id).get();
-        pizza.addIngredient(ing);
+    public String processPizzaOrder(@ModelAttribute("pizza") Pizza pizza, Model model, Principal principal) {
 
-        model.addAttribute("pizza", pizza);
-        Order order = new Order();
-        order.addPizza(pizza);
-        pizza.setPrice(pizza.calculatePrice());
-        pizza.setOrder(order);
-        order.setTotal(order.calculateTotal());
+        if (!pizzaRepository.existsById(pizza.getId())) {
 
-        if (principal!=null){
-            User currentUser = userRepository.findByUsername(principal.getName());
-            order.setUser(currentUser);
+            Order order = new Order();
+            order.addPizza(pizza);
+            pizza.setPrice(pizza.calculatePrice());
+            pizza.setOrder(order);
+            order.setTotal(order.calculateTotal());
+
+            if (principal != null) {
+                User currentUser = userRepository.findByUsername(principal.getName());
+                order.setUser(currentUser);
 //            order.setComplete(true);
+            }
+            orderRepository.save(order);
+        } else {
+
+            pizzaRepository.save(pizza);
         }
-        orderRepository.save(order);
+
+
         return "redirect:/";
     }
 
     @GetMapping("/cart")
-    public String loadUserCart(Model model, Principal principal){
+    public String loadUserCart(Model model, Principal principal) {
 
-        if (principal!=null) {
+        if (principal != null) {
             User currentUser = userRepository.findByUsername(principal.getName());
-            for (Order order : orderRepository.findAllByUserNull()){
+            for (Order order : orderRepository.findAllByUserNull()) {
                 order.setUser(currentUser);
                 orderRepository.save(order);
             }
             model.addAttribute("user", currentUser);
         } else {
 //            model.addAttribute("guestOrders",orderRepository.findAllByComplete(false));
-            model.addAttribute("guestOrders",orderRepository.findAllByUserNull());
+            model.addAttribute("guestOrders", orderRepository.findAllByUserNull());
         }
         return "cart";
     }
--- src/main/java/com/example/demo/PizzaRepository.java
+++ src/main/java/com/example/demo/PizzaRepository.java
@@ -3,5 +3,6 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PizzaRepository extends CrudRepository<Pizza, Long> {
+    boolean existsById(long id);
 }
 
